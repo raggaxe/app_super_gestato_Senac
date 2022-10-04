8874d5327cd1073f79186d1e550cd167317cdcf9
Rafael Almeida <raggaxe@gmail.com>
2022-10-04 19:55:28 -0300

    new version

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/','PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+
+
+
 Route::get('/contatos', 'ContatoController@principal')->name('site.contatos');
+
+
+
 Route::get('/login',function(){return 'Login';})->name('site.login');
 
 
